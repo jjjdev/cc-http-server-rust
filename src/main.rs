5193eee27236e7c5bd78cee69b78fba56c16d594
J Jackson <jjjacksondev@gmail.com>
2024-04-07 19:43:39 -0400

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 // Uncomment this block to pass the first stage
 // use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream);
+            }
+            Err(e) => { 
+                // Connection failed
+                println!("Error: {}", e);
+            }
+        }
+    }
 
     // Uncomment this block to pass the first stage
     //
@@ -19,4 +34,8 @@ fn main() {
     //         }
     //     }
     // }
+}
+
+fn handle_connection(stream: TcpStream) {
+    // do nothing....so far
 }
